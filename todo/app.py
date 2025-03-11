2130479d5e96db9df9e8d302d1035b630b075ccf
*hello* <mateo.moligon@gmail.com>
2025-03-11 11:15:48 -0500

    class Todo finished

--- todo/app.py
+++ todo/app.py
@@ -1,12 +1,10 @@
 from todo.model.todo import TodoBook
 from todo.view.ui_console import Console
 
-
 def main():
     book: TodoBook = TodoBook()
     ui: Console = Console(book)
     ui.app_loop()
-
 
 if __name__ == "__main__":
     main()
--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,49 @@
 # TODO: Add code here
+
+# 1
+class Todo:
+    def __init__(self, code_id: int, title: str, description: str):
+        self.code_id: int = code_id
+        self.title: str = title
+        self.description: str = description
+        self.completed: bool = False
+        self.tags: list[str] = []
+
+# 2
+    def mark_completed(self):
+        self.completed: bool = True
+
+# 3
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+# 4
+    def __str__(self) -> str:
+        return f"{self.code_id} - {self.title}"
+
+# 5
+class TodoBook:
+    def __init__(self):
+        self.todos: dict = {}
+
+# 6
+    def add_todo(self, title: str, description: str) -> int:
+        #
+        #
+        #
+        #
+
+"""
+# 7
+    def pending_todos(self) -> list:
+        #
+
+# 8
+    def compled_todos(self) -> list:
+        #
+
+# 9
+    def tags_todo_count(self) -> dict[int]:
+        #
+"""
